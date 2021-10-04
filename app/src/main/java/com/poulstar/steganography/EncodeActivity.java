49724b253c0d0791122d78c2a09d1f6279fe472f
rasulalizadeh <mohammad.alizadeh12@gmail.com>
2021-10-04 16:15:33 +0330

    added permission check to encoder activity

--- app/src/main/java/com/poulstar/steganography/EncodeActivity.java
+++ app/src/main/java/com/poulstar/steganography/EncodeActivity.java
@@ -1,11 +1,16 @@
 package com.poulstar.steganography;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 public class EncodeActivity extends Activity {
@@ -13,6 +18,8 @@ public class EncodeActivity extends Activity {
     ImageView imgImage;
     Button btnSave, btnShare, btnCapture;
     EditText txtText;
+
+    private final int REQUEST_ID = 5;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,8 +30,59 @@ public class EncodeActivity extends Activity {
         txtText = findViewById(R.id.txtText);
         btnSave = findViewById(R.id.btnSave);
         btnShare = findViewById(R.id.btnShare);
-        btnShare = findViewById(R.id.btnCapture);
+        btnCapture = findViewById(R.id.btnCapture);
+
+        btnCapture.setOnClickListener(view -> {
+            capture();
+        });
+        btnShare.setOnClickListener(view -> {
+            share();
+        });
+        btnSave.setOnClickListener(view -> {
+            save();
+        });
+
+        checkPermissions();
     }
 
+    private void capture() {
 
+    }
+
+    private void share() {
+
+    }
+
+    private void save() {
+
+    }
+
+    private void checkPermissions() {
+        if(Build.VERSION.SDK_INT >= 26) {
+            if(checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
+                || checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                }, REQUEST_ID);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == REQUEST_ID) {
+            boolean result = true;
+            for (int gResult : grantResults) {
+                if(gResult != PackageManager.PERMISSION_GRANTED) {
+                    result = false;
+                }
+            }
+            if(!result) {
+                Intent intent = new Intent(EncodeActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        }
+    }
 }
