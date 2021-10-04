5a27c3f8d827b0229a42665c64f8a4aa2d844739
rasulalizadeh <mohammad.alizadeh12@gmail.com>
2021-10-04 15:58:32 +0330

    completed encode layout

--- app/src/main/java/com/poulstar/steganography/EncodeActivity.java
+++ app/src/main/java/com/poulstar/steganography/EncodeActivity.java
@@ -2,14 +2,26 @@ package com.poulstar.steganography;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 
 public class EncodeActivity extends Activity {
 
+    ImageView imgImage;
+    Button btnSave, btnShare;
+    EditText txtText;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.encode_layout);
+
+        imgImage = findViewById(R.id.imgView);
+        txtText = findViewById(R.id.txtText);
+        btnSave = findViewById(R.id.btnSave);
+        btnShare = findViewById(R.id.btnShare);
     }
 }
