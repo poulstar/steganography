afe05a8d26ff1772304c79e04e81000a65feefc4
rasulalizadeh <mohammad.alizadeh12@gmail.com>
2021-10-11 13:08:25 +0330

    Added file selector in decoder activity

--- app/src/main/java/com/poulstar/steganography/DecodeActivity.java
+++ app/src/main/java/com/poulstar/steganography/DecodeActivity.java
@@ -1,6 +1,10 @@
 package com.poulstar.steganography;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -9,12 +13,18 @@ import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 public class DecodeActivity extends Activity {
 
 
     ImageView imgPreview;
     Button btnLoad;
     TextView txtMessage;
+    Bitmap image;
+
+    private final int MEDIA_REQUEST_ID = 8;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,5 +34,35 @@ public class DecodeActivity extends Activity {
         imgPreview = findViewById(R.id.imgPreview);
         btnLoad = findViewById(R.id.btnLoad);
         txtMessage = findViewById(R.id.txtMessage);
+
+        btnLoad.setOnClickListener(v -> {
+            loadImage();
+        });
+    }
+
+    private void loadImage() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/png");
+        startActivityForResult(intent, MEDIA_REQUEST_ID);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == MEDIA_REQUEST_ID) {
+            if(resultCode == RESULT_OK) {
+                Uri uri = data.getData();
+                try {
+                    InputStream input = getContentResolver().openInputStream(uri);
+                    image = BitmapFactory.decodeStream(input);
+
+                    runOnUiThread(() -> {
+                        imgPreview.setImageBitmap(image);
+                    });
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
