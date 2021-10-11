ae4d867476c70319eedb7ae8c11eb397151e4d70
rasulalizadeh <mohammad.alizadeh12@gmail.com>
2021-10-11 12:57:44 +0330

    Completed decode layout

--- app/src/main/java/com/poulstar/steganography/DecodeActivity.java
+++ app/src/main/java/com/poulstar/steganography/DecodeActivity.java
@@ -2,14 +2,27 @@ package com.poulstar.steganography;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
 public class DecodeActivity extends Activity {
 
+
+    ImageView imgPreview;
+    Button btnLoad;
+    TextView txtMessage;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.decode_layout);
+
+        imgPreview = findViewById(R.id.imgPreview);
+        btnLoad = findViewById(R.id.btnLoad);
+        txtMessage = findViewById(R.id.txtMessage);
     }
 }
